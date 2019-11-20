30787aabd99dcbfb42d8cf1e8d5edf10e838c0af
18159761 <48758161+18159761@users.noreply.github.com>
2019-11-20 12:26:27 +0000

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,17 +1,17 @@
+
+--Easy Function 
 SELECT COUNT(Employee_ID)
 FROM employee;
 WHERE (department = 'Legal');
 
-SELECT COUNT (Artist_ID)
-FROM artist
-WHERE(Record_Label_ID=2);
 
 
-SELECT (record_label)
-FROM address
-WHERE (country_name='USA' AND NOT 'New York');
-
-
+--Harder function -- this is the fucntion that uses a 2 table join 
+SELECT recordlabel.label_name, address.country_name
+FROM recordlabel
+INNER JOIN address 
+ON recordlabel.Record_Label_ID.=address.Record_Label_ID
+WHERE(country_name='USA');
 
 
 
@@ -23,23 +23,26 @@ WHERE(country_name='USA' AND NOT 'New York');
 
 
 
-SELECT recordlabel.label_name, address.country_name
-FROM recordlabel
-INNER JOIN address 
-ON recordlabel.Record_Label_ID.=address.Record_Label_ID
-WHERE(country_name='USA' AND NOT 'New York');
+DROP TRIGGER IF EXISTS tr_ins_yeet;
 
 
-
-DROP TRIGGER IF EXISTS tr_up_yeet;
-
+--This is the first trigger
 CREATE TRIGGER tr_ins_yeet
 BEFORE INSERT ON artist
 FOR EACH ROW
 NEW.character_name = UPPER(new.character_name);
 
-CREATE TRIGGER tr_up_yeet
-BEFORE UPDATE ON artist
-FOR EACH ROW
-SET NEW.character_name = 
-LOWER(new.character_name);
+
+--
+
+CREATE PROCEDURE 
+AS 
+BEGIN
+SELECT name, Employee_ID FROM employee
+END
+
+
+CREATE PROCEDURE SelectAllCEO @Job nvarchar(30)
+AS
+SELECT * FROM employee WHERE department = @Job
+GO;
