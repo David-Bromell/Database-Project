9630fea4ef7ca736f810b3d73201187976253f7a
David-Bromell <davidbromell62@gmail.com>
2019-11-23 13:22:33 +0000

    Update views.sql
    Now includes index

--- views.sql
+++ views.sql
@@ -1,8 +1,15 @@
 
 #VIEW 1 - EMPLOYEE PUBLIC ACCESS
+
+
+
+
 CREATE VIEW employee_public AS
 SELECT name, age, department
 FROM employee;
+
+CREATE UNIQUE INDEX employee_index
+ON employee_public(age);
 
 SELECT age
 FROM employee_public
@@ -11,10 +18,14 @@ HAVING (employee_public.age)>16
 ORDER BY count(AGE) DESC;
 
 
-#VIEW 2 - SINGLE FOR PUBLIC ACCESS
+#VIEW 2 - SINGLE FOR PUBLIC ACCESs
+
 CREATE VIEW single_public AS
 SELECT song_name, song_length, explicit_
 FROM single;
+
+CREATE INDEX single_index
+ON single(explicit);
 
 SELECT explicit_
 FROM single_public
@@ -26,7 +37,10 @@ CREATE VIEW album_public AS
 SELECT album_name,album_length,explicit_
 FROM album;
 
+CREATE INDEX album_index
+ON album(album_length);
+
 SELECT album_length
 FROM album_public
 GROUP BY album_length
-HAVING(album_length>=30);
+HAVING(album_length>=30); 
